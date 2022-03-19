79d0e8644fcb109c28e460deb51cc30f4fff688a
dhopz <david_houpapa@hotmail.com>
2022-03-19 11:50:34 +0000

    vehicle extract added

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,7 +1,6 @@
 import os
 import csv
 import tempfile
-from turtle import down
 from zipfile import ZipFile
 
 import requests
@@ -14,6 +13,7 @@ source_path = f"{base_path}/data/source/analyst-challenge.zip"
 
 # Raw path where we want to extract the new .csv data
 drive_path = f"{base_path}/data/raw/new_drive.csv"
+vehicle_path = f"{base_path}/data/raw/new_vehicle.csv"
 
 def create_folder_if_not_exists(path):
     """
@@ -31,15 +31,14 @@ def save_drive_raw_data():
             source_path,
             "r",
         ) as zipfile:
-            names_list = zipfile.namelist()
-            print(names_list)
+            #names_list = zipfile.namelist()
             csv_file_path = zipfile.extract('analyst-challenge-data/drive.csv', path=dirpath)
             # Open the CSV file in read mode
             with open(csv_file_path, mode="r", encoding="windows-1252") as csv_file:
                 reader = csv.DictReader(csv_file)
 
                 row = next(reader)  # Get first row from reader
-                print("[Extract] First row example:", row)
+                #print("[Extract] First row example:", row)
 
                 # Open the CSV file in write mode
                 with open(
@@ -68,9 +67,64 @@ def save_drive_raw_data():
                         # Write all rows in file
                         writer.writerow(row)
 
+def save_vehicle_raw_data():
+    """
+    Save new raw data from the source
+    """
+    create_folder_if_not_exists(drive_path)
+    with tempfile.TemporaryDirectory() as dirpath:
+        with ZipFile(
+            source_path,
+            "r",
+        ) as zipfile:
+            #names_list = zipfile.namelist()
+            csv_file_path = zipfile.extract('analyst-challenge-data/vehicle.csv', path=dirpath)
+            # Open the CSV file in read mode
+            with open(csv_file_path, mode="r", encoding="windows-1252") as csv_file:
+                reader = csv.DictReader(csv_file)
+
+                row = next(reader)  # Get first row from reader
+                #print("[Extract] First row example:", row)
+
+                # Open the CSV file in write mode
+                with open(
+                    vehicle_path,
+                    mode="w",
+                    encoding="windows-1252"
+                ) as csv_file:
+                    # Rename field names so they're ready for the next step
+                    fieldnames = {
+                        'vehicle_spec_id':'vehicle_spec_id',
+                        'year':'year',
+                        'make':'make',
+                        'Model':'model',
+                        'drivetrain':'drivetrain',
+                        'max_torque':'max_torque',
+                        'max_horsepower':'max_horsepower',
+                        'max_horsepower_rpm':'max_horsepower_rpm',
+                        'max_torque_rpm':'max_torque_rpm',
+                        'engine_displacement':'engine_displacement',
+                        'fuel_type':'fuel_type',
+                        'fuel_tank_capacity':'fuel_tank_capacity',
+                        'fuel_economy_city':'fuel_economy_city',
+                        'fuel_economy_highway':'fuel_economy_highway',
+                        'cylinders':'cylinders',
+                        'forced_induction':'forced_induction',
+                        'device_generation':'device_generation'                        
+                    }
+                    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+                    # Write headers as first line
+                    writer.writerow(fieldnames)
+                    for row in reader:
+                        # Write all rows in file
+                        writer.writerow(row)
+
 # Main function called inside the execute.py script
 def main():
     print("[Extract] Start")
-    print(f"[Extract] Saving data from '{source_path}' to '{drive_path}'")
+    print(f"[Extract] Saving data from '{source_path}' to '{drive_path}' and '{vehicle_path}'")
     save_drive_raw_data()
+    print("[Extract] Drive data CSV complete")
+    save_vehicle_raw_data()
+    print("[Extract] Vehicle data CSV complete")
     print(f"[Extract] End")
