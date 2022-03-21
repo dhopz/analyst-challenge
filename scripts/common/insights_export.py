43d53a68084b5ec9d21eac66ee090d43e9f4f655
dhopz <david_houpapa@hotmail.com>
2022-03-21 15:15:14 +0000

    excel writer added

---
+++ scripts/common/insights_export.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+import os
+
+from base import session
+from tables import DriveAll
+import xlsxwriter
+
+# Settings
+base_path = os.path.abspath(__file__ + "/../../../")
+ref_month = datetime.today().strftime("%Y-%m")
+
+if __name__ == "__main__":
+    data = session.execute(
+        '''SELECT * FROM drive
+        WHERE trip_id = '00922df3be5a4589ab385d0c2da2dd81';'''
+        ).all()
+    ref_month = datetime.today().strftime("%Y-%m")
+    
+    # Create the workbook
+    workbook = xlsxwriter.Workbook(
+        f"{base_path}/data/insights/SingleTripId.xlsx"
+    )
+    
+    # Add a new worksheet
+    worksheet = workbook.add_worksheet()
+    worksheet.set_column("A:L", 12)
+
+    #print(data)
+    
+    # Add the table with all results in the newly created worksheet
+    worksheet.add_table(
+        "A2:L2900",
+        {
+            "data": data,
+            "columns": [
+                {"header": 'id'},
+                {"header": 'trip_id'},
+                {"header": 'datetime'},
+                {"header": 'vehicle_spec_id'},
+                {"header": 'engine_coolant_temp'},
+                {"header": 'eng_load'},
+                {"header": 'fuel_level'},
+                {"header": 'iat'},
+                {"header": 'rpm'},
+                {"header": 'lat'},
+                {"header": 'long'},
+                {"header": 'velocity'},
+            ],
+        },
+    )
+    workbook.close()
+    
+    print("Data exported:",  f"{base_path}/data/insights/SingleTripId.xlsx")
